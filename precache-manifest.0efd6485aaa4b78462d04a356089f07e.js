7da0b3f4aa0c35016a010b741cbfa6aed09cf4a3
Shane Lennon <shane.michael.lennon@gmail.com>
2019-04-03 17:02:26 -0500

    Updates

---
+++ precache-manifest.0efd6485aaa4b78462d04a356089f07e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.895984e4.js"
+  },
+  {
+    "revision": "d99fefaa6415adfab0e3",
+    "url": "/clicky-game/static/js/main.558f9fdd.chunk.js"
+  },
+  {
+    "revision": "31d68d79c0937db6dad4",
+    "url": "/clicky-game/static/js/2.fb03e36a.chunk.js"
+  },
+  {
+    "revision": "d99fefaa6415adfab0e3",
+    "url": "/clicky-game/static/css/main.9911ce67.chunk.css"
+  },
+  {
+    "revision": "cd615ab19f33ef248ee04cf141678e42",
+    "url": "/clicky-game/index.html"
+  }
+];
